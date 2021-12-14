1fb8c73942d07bbe0585b28741caa2a9cda2ad3a
Burak Hamza <thedatcal@gmail.com>
2021-12-14 17:40:39 +0300

    add log level with env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.ENV === 'true'
+        ? ['debug', 'error', 'log', 'verbose', 'warn']
+        : ['error', 'warn'],
+  });
+  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
