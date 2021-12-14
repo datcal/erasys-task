e4724ae17dba4177a72a1a5a637707a617b02610
Burak Hamza <thedatcal@gmail.com>
2021-12-14 17:40:16 +0300

    import db and other configs

---
+++ config/configuration.ts
@@ -0,0 +1,19 @@
+export default () => ({
+  database: {
+    client: process.env.DATABASE_CLIENT || 'mysql',
+    host: process.env.DATABASE_HOST,
+    name: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+  },
+  api: {
+    validation: process.env.VALIDATION_API,
+    compromised: process.env.COMPROMISED_API,
+  },
+  app: {
+    debug: process.env.APP_DEBUG || false,
+    logType: process.env.APP_LOG_TYPE || 'logger',
+    port: parseInt(process.env.PORT, 10) || 3000,
+  },
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PasswordModule } from './password/password.module';
 import validation from 'config/validation';
+import configuration from 'config/configuration';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [validation],
+      load: [validation, configuration],
       isGlobal: true,
     }),
     PasswordModule,
