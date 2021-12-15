443b179afdd3c312dfe0f957a307d660f77be5a4
Burak Hamza <thedatcal@gmail.com>
2021-12-15 20:16:30 +0300

    add api e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,20 +1,66 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
+import { PasswordService } from '../src/password/password.service';
+import { PasswordModule } from '../src/password/password.module';
+import validation from '../config/validation';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let httpServer: any;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        PasswordModule,
+        ConfigModule.forRoot({
+          load: [validation],
+          isGlobal: true,
+          cache: false,
+        }),
+      ],
+      providers: [PasswordService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    httpServer = app.getHttpServer();
   });
 
-  it('/ (GET)', async () =>
-    request(app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
+  describe('checkPassword', () => {
+    it('should return invalid - min 5', async () => {
+      const response = await request(httpServer)
+        .post('/passwords')
+        .send({ password: '1Acb' });
+
+      expect(response.status).toBe(400);
+    });
+    it('should return invalid - minimum 1 upppercase', async () => {
+      const response = await request(httpServer)
+        .post('/passwords')
+        .send({ password: '1acba' });
+
+      expect(response.status).toBe(400);
+    });
+    it('should return invalid -  3 and more repeating consecutive characters', async () => {
+      const response = await request(httpServer)
+        .post('/passwords')
+        .send({ password: '1acbaaa' });
+
+      expect(response.status).toBe(400);
+    });
+    it('should return invalid -  minimum 1 number', async () => {
+      const response = await request(httpServer)
+        .post('/passwords')
+        .send({ password: 'Cacbaa' });
+      expect(response.status).toBe(400);
+    });
+    it('should return valid', async () => {
+      const response = await request(httpServer)
+        .post('/passwords')
+        .send({ password: 'abC12' });
+      expect(response.status).toBe(204);
+    });
+  });
 });
